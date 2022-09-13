e2ebd59bb546cb0da4892e93d1ad25ffc1685508
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-09-13 22:37:37 +0600

    Add files via upload

---
+++ Linked_List/search_by_value_unique.cpp
@@ -0,0 +1,130 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *Next;
+
+    Node(int value)
+    {
+        this->value = value;
+        this->Next = NULL;
+    }
+};
+
+void insertAtHead(Node *&head, int value);
+void insertAtTail(Node *&head, int value);
+int searchByValueUnique(Node *&head, int value);
+void displayLinkedList(Node *head);
+
+void insertAtHead(Node *&head, int value)
+{
+    Node *newNode = new Node(value);
+    newNode->Next = head;
+    head = newNode;
+}
+void insertAtTail(Node *&head, int value)
+{
+    Node *newNode = new Node(value);
+    if (head == NULL)
+    {
+        head = newNode;
+        return;
+    }
+    Node *temp = head;
+    while (temp->Next != NULL)
+    {
+        temp = temp->Next;
+    }
+    temp->Next = newNode;
+}
+
+
+int searchByValueUnique(Node *&head, int value)
+{
+    if (head == NULL)
+    {
+        return -1;
+    }
+    Node *temp = head;
+    int i = 1;
+    while (temp->value != value)
+    {
+        if(temp->Next == NULL)return -1;
+        temp = temp->Next;
+        i++;
+    }
+    return i;
+}
+
+
+
+void displayLinkedList(Node *head)
+{
+    if (head == NULL)
+    {
+        cout << "No value in the list." << endl;
+    }
+    while (head != NULL)
+    {
+        cout << head->value;
+        if (head->Next != NULL)
+        {
+            cout << " -> ";
+        }
+        head = head->Next;
+    }
+    cout << endl
+         << endl;
+}
+
+int main()
+{
+    Node *head = NULL;
+    cout << "Choice 1 : Insert at head of linked list." << endl
+         << "Choice 2 : Insert at tail of linked list." << endl
+         << "Choise 3 :Search a value (Unique List)" << endl
+         << "Choice 0 : Exit." << endl;
+    int value, pos;
+    int choice;
+    cout << "Enter Your Choice : ";
+    cin >> choice;
+    while (choice != 0)
+    {
+        switch (choice)
+        {
+        case 1:
+            cout << "Enter The Value : ";
+            cin >> value;
+            insertAtHead(head, value);
+            break;
+        case 2:
+            cout << "Enter The Value : ";
+            cin >> value;
+            insertAtTail(head, value);
+            break;
+
+        case 3:
+            cout << "Enter The Value : ";
+            cin >> value;
+            pos = searchByValueUnique(head, value);
+            if(pos != -1)
+            cout << "The value in postion : " << pos << endl;
+            else cout<<"The value in not present in the list."<<endl;
+            break;
+
+        default:
+            break;
+        }
+        cout << "Enter Your Choice : ";
+        cin >> choice;
+    }
+    cout << endl;
+    cout << "Linked List : ";
+    displayLinkedList(head);
+    cout << endl;
+
+    return 0;
+}
