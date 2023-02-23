ce3af612f752a12da7c741538707a1954d50fbc4
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2023-02-23 07:05:04 +0600

    Update Vector_traversing_4_way.cpp
    traverse like an array

--- STL/Vector_traversing_4_way.cpp
+++ STL/Vector_traversing_4_way.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int main(){
   vector<int> vec(5,-2);
+  // traverse as an array
+  for(int i = 0;i<vec.size();i++){
+    cout<<vec[i]<<" ";
+  }
+
+  cout<<endl<<endl;
+
 
 
   return 0;
