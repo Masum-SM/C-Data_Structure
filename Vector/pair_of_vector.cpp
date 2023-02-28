02176058e56ce3e55bb1003268f32a02f8b4ebc6
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2023-02-28 12:38:35 +0600

    input output

--- Vector/pair_of_vector.cpp
+++ Vector/pair_of_vector.cpp
@@ -1,2 +1,18 @@
 // A vector that contain one or more pairs,
 // A pair can contain two value , maybe same type of different type.
+
+
+/* 
+INPUT:
+3     
+1 2
+0 0
+3 1
+
+OUTPUT:
+Size: 3
+First value : 1 Second value : 2
+First value : 0 Second value : 0
+First value : 3 Second value : 1
+
+*/
