41a204a11e034eea89478af3fd2e5e52332620db
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2023-02-28 11:53:32 +0600

    concept about vector

---
+++ Vector/vector_of_vector.cpp
@@ -0,0 +1,3 @@
+// In vector of vector, the number of row is dynamic and number of column is also dynamic. 
+// row and column both are dynamic bcz both are vector.
+// A vector contain other vectors.
