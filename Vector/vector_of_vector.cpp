fdde1e57710aef8587ba2811e77826924201b5d2
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2023-02-28 11:54:00 +0600

    input output

--- Vector/vector_of_vector.cpp
+++ Vector/vector_of_vector.cpp
@@ -1,3 +1,28 @@
 // In vector of vector, the number of row is dynamic and number of column is also dynamic. 
 // row and column both are dynamic bcz both are vector.
 // A vector contain other vectors.
+
+
+/*
+INPUT: 
+4
+3
+9 8 7
+1
+10
+4
+2 5 3 0
+2
+11 22
+
+OUTPUT:
+Size : 3
+9 8 7
+Size : 1
+10
+Size : 4
+2 5 3 0
+Size : 2
+11 22
+
+ */
