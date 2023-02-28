1101614c3c181d190bf51590dbf832791620a072
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2023-02-28 11:39:42 +0600

    Print value of each vector

--- Vector/Array_of_vector.cpp
+++ Vector/Array_of_vector.cpp
@@ -24,6 +24,16 @@ Size : 4
 #include<bits/stdc++.h>
 using namespace std;
 
+// ==============================> Print Vector <==========================
+void printVector(vector<int> v){
+    int s = v.size();
+    cout<<"Size : "<<s<<endl;
+    for(auto element : v){
+        cout<<element<<" ";
+    }
+    cout<<endl;
+}
+
 
 int main(){
     int num_of_vector;
