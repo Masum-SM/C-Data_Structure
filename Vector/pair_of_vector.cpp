cea9bafc628d54daf585910936c4a422e4f18cfd
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2023-02-28 12:39:11 +0600

    implement pair of vector

--- Vector/pair_of_vector.cpp
+++ Vector/pair_of_vector.cpp
@@ -16,3 +16,29 @@ First value : 0 Second value : 0
 First value : 3 Second value : 1
 
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+
+    vector< pair<int,int> >PV ;           // vector with 0 size which contain pairs.
+    int n;                                // number of pairs contain the vector.
+    cin>>n;
+    for(int i = 0 ; i < n ; i++){
+        int first_value ,second_value;    // first value in each pair
+        cin>>first_value>>second_value;   // second value in each pair
+        
+
+        //================> two way to push_back pair in vector <====================
+        PV.push_back({first_value,second_value});
+        // PV.push_back(make_pair(first_value,second_value));
+    }
+
+
+    printVector(PV);
+
+    return 0;
+}
