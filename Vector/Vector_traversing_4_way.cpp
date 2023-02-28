241db0b7a6840056751982c92d76c91d185fd64c
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2023-02-28 11:37:00 +0600

    Update Vector_traversing_4_way.cpp

--- Vector/Vector_traversing_4_way.cpp
+++ Vector/Vector_traversing_4_way.cpp
@@ -1,1 +1,35 @@
 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  vector<int> vec(5,-2);
+  // traverse as an array
+  for(int i = 0;i<vec.size();i++){
+    cout<<vec[i]<<" ";
+  }
+
+  cout<<endl<<endl;
+  // vector traversing using iterator.
+  vector<int> ::iterator it;
+  for(it=vec.begin(); it != vec.end();it++){
+    cout<<*it<<" ";
+  }
+
+  cout<<endl<<endl;
+
+  // vector traversing using auto keyword
+  for(auto element : vec){
+    cout<<element<<" ";
+  }
+  cout<<endl<<endl;
+
+  // vector traversing using auto and iterator
+  for(auto itr = vec.begin(); itr != vec.end(); itr++){
+    cout<< *itr<<" ";
+  }
+
+
+  return 0;
+}
