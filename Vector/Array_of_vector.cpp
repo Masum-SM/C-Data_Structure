ceae7b66d1c7bd8f5c34b1ef16da5209f2c4de0f
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2023-02-28 11:39:20 +0600

    implement array of vector

--- Vector/Array_of_vector.cpp
+++ Vector/Array_of_vector.cpp
@@ -19,3 +19,33 @@ Size : 4
 0 0 0 0
 
  */
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int num_of_vector;
+    cin>>num_of_vector;
+
+    vector<int> v[num_of_vector];                    // An array of vector which contain vector.
+    for(int i = 0 ; i < num_of_vector ; i++){       // This loop for taking every vector.
+
+        int num_of_value_each_vector;               // number of value for individual vector      
+        cin>>num_of_value_each_vector;
+
+        for(int j = 0; j<num_of_value_each_vector;j++){
+            int value;                            // value that will store in individual vector
+            cin>>value;
+            v[i].push_back(value);
+        }
+        
+    }
+
+    for(int i = 0 ; i < num_of_vector ; i++){
+        printVector(v[i]);                    // it will pass a single vector.
+    }
+
+    return 0;
+}
